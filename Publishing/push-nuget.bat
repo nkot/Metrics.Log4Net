8144d719fb1899f52945c9a96d817786ec21508f
nkot <nazar.kotylo@gmail.com>
2014-07-25 16:06:37 +0300

    Incremented nuget package version to 0.1.11

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,1 +1,1 @@
-..\.nuget\NuGet.exe push Metrics.Log4Net.0.1.9.nupkg
+..\.nuget\NuGet.exe push Metrics.Log4Net.0.1.11.nupkg
--- create-nuget.bat
+++ create-nuget.bat
@@ -9,7 +9,6 @@ md .\Publishing\lib\net45
 copy .\Src\Metrics.Log4Net\bin\Release\metrics.log4net.dll .\Publishing\lib\net45\
 copy .\Src\Metrics.Log4Net\bin\Release\metrics.log4net.xml .\Publishing\lib\net45\
 copy .\Src\Metrics.Log4Net\bin\Release\metrics.log4net.pdb .\Publishing\lib\net45\
-copy .\Src\Metrics.Log4Net\bin\Release\metrics.log4net.config .\Publishing\lib\net45\
 
 .\.nuget\NuGet.exe pack .\Publishing\Metrics.Log4Net.nuspec -OutputDirectory .\Publishing
 if %errorlevel% neq 0 exit /b %errorlevel%
